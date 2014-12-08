c6b358fb355bcbc5028c4f24b716b6fe21ffb992
JordanByrd <jordan.byrd@carestream.com>
2014-12-08 17:07:23 -0500

    remove some comments

--- ixio.py
+++ ixio.py
@@ -11,8 +11,3 @@ class IxioCommand(sublime_plugin.TextCommand):
 			r = requests.post('http://ix.io', data={"f:1":s})
 			sublime.set_clipboard(r.text)
 			print('ix.io post success: ' + r.text)
-	# here's how it's gonna work
-	# first, we need to either get the selection or the window if there is no selection
-	# next we need to make an http request with the text as in the body
-	# when that returns we need to copy the url in the response to the clipboard
-# notify the user of success
