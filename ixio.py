5ac7f8b5af97d9932a5d6bacfefbaf23874738c0
JordanByrd <jordan.byrd@carestream.com>
2015-01-05 15:39:56 -0500

    gitignore, pep8 format, basic auth first pass

--- ixio.py
+++ ixio.py
@@ -15,63 +15,73 @@ statusId = None
 
 class IxioCommand(sublime_plugin.TextCommand):
 
-	def run(self, edit):
-		for region in self.view.sel():
-			if region.empty():
-				s = self.view.substr(sublime.Region(0, self.view.size()))
-			else:
-				s = self.view.substr(region)
-			if username is None:
-				url = "http://ix.io"
-				data = urllib.parse.urlencode({"f:1": s}).encode('utf8')
-				req = urllib.request.Request(url, data=data, headers={'content-type': 'application/x-www-form-urlencoded'})
-				response = urllib.request.urlopen(req)
-				page = response.read().decode('utf8')
-				print(page)			
-			else:
-				print("test")
-				#r = requests.post(
-				   #"http://ix.io", auth=(username, password), data={"f:1": s})
-			# sublime.set_clipboard(
-			#     re.search("(?P<url>https?://[^\s]+)", r.text).group("url"))
+    def run(self, edit):
+        for region in self.view.sel():
+            if region.empty():
+                s = self.view.substr(sublime.Region(0, self.view.size()))
+            else:
+                s = self.view.substr(region)
+            url = "http://ix.io"
+            data = urllib.parse.urlencode(
+                {"f:1": s}).encode('utf8')
+            if username is None:
+                req = urllib.request.Request(
+                    url, data=data, headers={'content-type': 'application/x-www-form-urlencoded'})
+                response = urllib.request.urlopen(req)
+                r = response.read().decode('utf8')
+                print(page)
+            else:
+                encode = username + ":" + password
+                encode = bytes(encode)
+                auth = base64.b64encode(encode).decode('ascii')
+                req = urllib.request.Request(
+                    url, data=data, headers={'content-type': 'application/x-www-form-urlencoded'})
+                req.add_header("Authorization", "Basic %s" % auth)
+                print(req.get_header("Authorization"))
+                response = urllib.request.urlopen(req)
+                page = response.read().decode('utf8')
+                print(page)
+            sublime.set_clipboard(
+                re.search("(?P<url>https?://[^\s]+)", r.text).group("url"))
+
 
 class StatusCommand(sublime_plugin.TextCommand):
 
-	def run(self, edit):
-		print(username)
-		r = urllib.request.urlopen("http://ix.io/user/" + username)
-		html = r.read().decode()
-		content = html2text.html2text(html)
-		content += "\n NOTE: select a line and right-click to open that paste in a browser"
-		newWindow = self.view.window().new_file()
-		activeView = self.view.window().active_view()
-		global statusId
-		statusId = activeView.id()
-		activeView.set_scratch(True)
-		activeView.set_name("Your Pastes")
-		activeView.replace(edit=edit, r=sublime.Region(0, 0), text=content)
-		activeView.sel().clear()
+    def run(self, edit):
+        print(username)
+        r = urllib.request.urlopen("http://ix.io/user/" + username)
+        html = r.read().decode()
+        content = html2text.html2text(html)
+        content += "\n NOTE: select a line and right-click to open that paste in a browser"
+        newWindow = self.view.window().new_file()
+        activeView = self.view.window().active_view()
+        global statusId
+        statusId = activeView.id()
+        activeView.set_scratch(True)
+        activeView.set_name("Your Pastes")
+        activeView.replace(edit=edit, r=sublime.Region(0, 0), text=content)
+        activeView.sel().clear()
 
 
 class DoubleClickCommand(sublime_plugin.TextCommand):
 
-	def run_(self, view, args):
-		if self.view.id() is statusId:
-			jumper = self.view.line(self.view.sel()[0])
-			jumper = self.view.substr(jumper)
-			if "[[r]]" in jumper and "[[h]]" in jumper:
-				goTo = jumper.partition(" ")[0]
-				webbrowser.open_new("http://ix.io/" + goTo)
-				print(goTo)
-			return
-		else:
-			return
+    def run_(self, view, args):
+        if self.view.id() is statusId:
+            jumper = self.view.line(self.view.sel()[0])
+            jumper = self.view.substr(jumper)
+            if "[[r]]" in jumper and "[[h]]" in jumper:
+                goTo = jumper.partition(" ")[0]
+                webbrowser.open_new("http://ix.io/" + goTo)
+                print(goTo)
+            return
+        else:
+            return
 
 
 def plugin_loaded():
-	global settings
-	global username
-	global password
-	settings = sublime.load_settings(SETTINGS_FILE)
-	username = settings.get("ixioUn")
-	password = settings.get("ixioPw")
+    global settings
+    global username
+    global password
+    settings = sublime.load_settings(SETTINGS_FILE)
+    username = settings.get("ixioUn")
+    password = settings.get("ixioPw")
